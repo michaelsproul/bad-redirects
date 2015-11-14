8d431f2f3d11dd927e2c210596fb98e66a07c7c2
Michael Sproul <micsproul@gmail.com>
2015-11-14 19:06:39 +1100

    More generic badness (lots of status codes).

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bad Redirects
+
+In the process of trying to break Telegram's URL previewer I wrote this web-app that redirects
+requests indefinitely.
+
+```
+$ cargo run
+```
+
+Visit:
+
+```
+http://localhost:3000/302
+```
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 extern crate iron;
+extern crate mount;
 
 use iron::prelude::*;
-use iron::status;
+use iron::status::Status;
 use iron::modifiers::Header;
 use iron::headers::Location;
+use mount::{Mount, OriginalUrl};
+
+// Simple macro to generate handler closures.
+macro_rules! handle {
+    ($code:expr) => {
+        |req: &mut Request| { handle_redirect($code, req) }
+    }
+}
+
+// Status-code generic redirect handler.
+// Adds "hehe" to the end of the current URL path and returns a response with
+// the Location header set to this new path.
+fn handle_redirect(status_code: Status, req: &mut Request) -> IronResult<Response> {
+    let mut path = String::new();
+
+    // Add the top-level part of the URL (the redirect number).
+    let orig_url = req.extensions.get::<OriginalUrl>().unwrap();
+
+    path.push_str("/");
+    path.push_str(&orig_url.path[0]);
+    path.push_str("/");
+
+    // Add all the pieces of the redirect path so far.
+    for piece in req.url.path.iter() {
+        path.push_str(piece);
+        path.push_str("/");
+    }
+
+    // Add another piece of redirect path.
+    path.push_str("hehe");
+
+    Ok(Response::with((status_code, Header(Location(path)))))
+}
 
 fn main() {
-    Iron::new(|req: &mut Request| {
-	let mut path = String::new();
-	for piece in req.url.path.iter() {
-		path.push_str(piece);
-		path.push_str("/");
-	}
-	path.push_str("hehe");
+    use iron::status::Status::*;
 
-        Ok(Response::with((status::SeeOther, Header(Location(path)))))
-    }).http("0.0.0.0:3000").unwrap();
+    let mut mount = Mount::new();
+    mount.mount("/300/", handle!(MultipleChoices));
+    mount.mount("/301/", handle!(MovedPermanently));
+    mount.mount("/302/", handle!(Found));
+    mount.mount("/303/", handle!(SeeOther));
+    mount.mount("/304/", handle!(NotModified));
+    mount.mount("/305/", handle!(UseProxy));
+    mount.mount("/307/", handle!(TemporaryRedirect));
+    mount.mount("/308/", handle!(PermanentRedirect));
+
+    Iron::new(mount).http("0.0.0.0:3000").unwrap();
 }
